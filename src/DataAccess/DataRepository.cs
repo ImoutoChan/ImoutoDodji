3908c07e6ec7fc2f51ac55917015c655e49881d6
ImoutoChan <kirirrin@gmail.com>
2016-11-03 22:44:15 +0300

    DB model

--- src/DataAccess/DataRepository.cs
+++ src/DataAccess/DataRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,9 +15,37 @@ namespace DataAccess
             }
         }
 
-        public async Task<Tag> GetTag(string nameSpace, string name)
+        public async Task<Tag> GetTag(string nameSpaceName, string tagName)
         {
-            
+            using (var db = new DataContext())
+            {
+                var dbNameSpace = db.Namespaces.FirstOrDefaultAsync(x => x.Name == nameSpaceName);
+                if (dbNameSpace == null)
+                {
+                    await CreateNamespace(nameSpaceName, db);
+                }
+
+                var dbTag = db.Tags.FirstOrDefaultAsync(x => x.Name == tagName);
+                if (dbTag == null)
+                {
+                    await CreateTag(tagName, db);
+                }
+            }
         }
+
+        #region Private methods
+        
+        private async Task<Namespace> CreateNamespace(string nameSpaceName, DataContext db)
+        {
+            var nameSpace = await db.Namespaces.AddAsync(new Namespace { Name = nameSpaceName });
+            return nameSpace.Entity;
+        }
+
+        private Tag CreateTag(string tagName, DataContext db)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
--- src/DataAccess/Models/DataContext.cs
+++ src/DataAccess/Models/DataContext.cs
@@ -1,32 +1,159 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccess.Models
 {
     public class DataContext : DbContext
     {
         public DbSet<Tag> Tags { get; set; }
+
         public DbSet<Namespace> Namespaces { get; set; }
+
+        public DbSet<DestinationFolder> DestinationFolders { get; set; }
+
+        public DbSet<SourceFolder> SourceFolders { get; set; }
+
+        public DbSet<Collection> Collection { get; set; }
+
+        public DbSet<Gallery> Galleries { get; set; } 
+
+        public DbSet<BindedTag> BindedTags { get; set; } 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=./data.db");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Namespace>()
+                .HasIndex(ns => ns.Name)
+                .IsUnique(true);
+            modelBuilder.Entity<Tag>()
+                .HasIndex(ns => ns.Name)
+                .IsUnique(true);
+        }
     }
 
     public class EntityBase
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 
     public class Namespace : EntityBase
     {
+        [Required]
         public string Name { get; set; }
+
+
+        public List<BindedTag> BindedTags { get; set; }
     }
 
     public class Tag : EntityBase
     {
+        [Required]
         public string Name { get; set; }
+
+
+        public List<BindedTag> BindedTags { get; set; }
+    }
+
+    public class DestinationFolder : EntityBase
+    {
+        [Required]
+        public string Path { get; set; }
+
+
+
+        [Required]
+        public int CollectionId { get; set; }
+
+        public Collection Collection { get; set; }
+    }
+
+    public class SourceFolder : EntityBase
+    {
+        [Required]
+        public string Path { get; set; }
+
+
+
+        [Required]
+        public int CollectionId { get; set; }
+
+        public Collection Collection { get; set; }
+    }
+
+    public class Collection : EntityBase
+    {
+        [Required]
+        public string Name { get; set; }
+
+
+
+        public List<SourceFolder> SourceFolders { get; set; } 
+
+        public DestinationFolder DestinationFolder { get; set; }
+    }
+
+    public enum StorageType
+    {
+        Archive,
+        Folder
+    }
+
+    public class Gallery : EntityBase
+    {
+        [Required]
+        public string Path { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public long FileSize { get; set; }
+
+        [Required]
+        public int FilesCount { get; set; }
+
+        [Required]
+        public string PreviewPath { get; set; }
+
+        [Required]
+        public string Md5 { get; set; }
+
+        [Required]
+        public StorageType StorageType { get; set; }
+
+
+        public List<BindedTag> BindedTags { get; set; }
+
+        [Required]
+        public int CollectionId { get; set; }
+
+        public Collection Collection { get; set; }
+    }
+
+    public class BindedTag
+    {
+        public string Value { get; set; }
+
+
+
+        [Required]
+        public int GalleryId { get; set; }
+
+        public Gallery Gallery { get; set; }
+
+        [Required]
+        public int TagId { get; set; }
+
+        public Tag Tag { get; set; }
+
+        [Required]
+        public int NamespaceId { get; set; }
+
+        public Namespace Namespace { get; set; }
     }
 }
