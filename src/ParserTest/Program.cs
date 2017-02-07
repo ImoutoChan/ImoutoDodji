2be84facf42219faab486acf3015e44b9949ea39
ImoutoChan <kirirrin@gmail.com>
2017-02-07 18:44:52 +0300

    updated to vs2017

--- src/ParserTest/Program.cs
+++ src/ParserTest/Program.cs
@@ -34,7 +34,7 @@ namespace ParserTest
 
         private static async Task RunTests()
         {
-            await LocalDbTest();
+            //await LocalDbTest();
 
             //await ParserTest();
 
@@ -42,7 +42,7 @@ namespace ParserTest
 
             //await TestObserver();
 
-            //await TestParser();
+            await TestParser();
         }
 
 
